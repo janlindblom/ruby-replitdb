0d6f12da12a8d73628a886e0a56d87d65dca7b59
Jan Lindblom <janlindblom@fastmail.fm>
2021-04-20 21:58:54 +0300

    Initial commit.

---
+++ lib/ruby/replitdb.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "replitdb/version"
+
+module Ruby
+  module Replitdb
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/ruby/replitdb/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Ruby
+  module Replitdb
+    VERSION = "0.1.0"
+  end
+end
